3933f633335a181b4f43df5c1a21021e103654b4
vinicius.kroth@acad.pucrs.br <vinicius.kroth@acad.pucrs.br>
2018-09-21 01:42:11 -0300

    Add example server

---
+++ src/connection/PlayTicTacToe.java
@@ -0,0 +1,11 @@
+package connection;
+
+public class PlayTicTacToe
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        TicTacToeGUI game = new TicTacToeGUI();
+        game.run();
+    }
+}
+
